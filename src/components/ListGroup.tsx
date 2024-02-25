b59f229a0b737f11866c5e162dd364133b35342b
Dennis Nyarko <dennis_nyarko@outlook.com>
2024-02-25 10:58:05 +0000

    update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,10 +2,12 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
+  
+
   return (
     <>
       <h1>List</h1>
-      { items.length === 0 ? <p>No item found</p> : null }
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
