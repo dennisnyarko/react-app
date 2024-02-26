e428c60af16d2c069bed0ac9ad4ea92824a4235b
Dennis Nyarko <dennis_nyarko@outlook.com>
2024-02-26 01:00:10 +0000

    update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,11 +7,11 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log("Clicked")}
+            onClick={() => console.log(item, index)}
           >
             {item}
           </li>
