c856e8fcf0695f1ed00d4c67da8410d16c2f4a16
Dennis Nyarko <dennis_nyarko@outlook.com>
2024-02-27 22:01:00 +0000

    update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,11 +5,8 @@ import { useState } from 'react';
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  let selectedIndex = 0;
-  // Hook
-  const arr = useState(-1);
-  arr[0] // variable (selectedIndex)
-  arr[1] // update function
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   
   //event handler
   //const handleClick = (event: MouseEvent) => console.log(event)
@@ -23,7 +20,7 @@ function ListGroup() {
           <li
             className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
             key={item}
-            onClick={() => { selectedIndex = index;}}
+            onClick={() => { setSelectedIndex(index);}}
           >
             {item}
           </li>
