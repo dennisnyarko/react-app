febd9ed7c9e18822016a78e4c7ddb16a71c9ab00
Dennis Nyarko <dennis_nyarko@outlook.com>
2024-02-29 22:44:59 +0000

    update component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,15 @@
 //import { MouseEvent } from 'react';
 
-import { useState } from 'react';
+import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-
-function ListGroup({ items, heading }: Props) {
-  
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  
   //event handler
   //const handleClick = (event: MouseEvent) => console.log(event)
 
@@ -22,9 +20,16 @@ function ListGroup({ items, heading }: Props) {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => { setSelectedIndex(index);}}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
