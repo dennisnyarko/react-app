3d5d13ebf62ad1e8eec7f0ef71a1f38b06c70df0
Dennis Nyarko <dennis_nyarko@outlook.com>
2024-03-20 23:12:50 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Button from './components/Button';
 function App() {
   return (
     <div>
-      <Button color="secondary" onClick={() => console.log('Clicked')}>My Button</Button>
+      <Button onClick={() => console.log('Clicked')}>My Button</Button>
     </div>
   );
 }
