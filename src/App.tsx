54c7a2c2cf38443f5ddef4f3a5953afaf3b56631
Dennis Nyarko <dennis_nyarko@outlook.com>
2024-03-21 00:09:52 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React , { useEffect, useState} from "react";
-import Button from './components/Button';
-import Alert from './components/Alert';
+import React, { useEffect, useState } from "react";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
 
-
   return (
     <div>
-      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>My Button</Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,14 +1,21 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 interface Props {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className="alert alert-primary alert-dismissible">{children}
-      <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss="alert" aria-label="Close"></button>
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
     </div>
-  )
-}
+  );
+};
 
 export default Alert;
